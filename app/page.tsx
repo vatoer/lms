e72bc8b30d52424f0cdb9df281d2394c0b78d7d4
Fathur Rohman <vatoer.ckplus@gmail.com>
2023-11-11 20:20:07 +0100

    basic shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-medium text-sky-700 ">hello</p>
-    )
+    <div>
+      <p className="text-3xl font-medium text-sky-700 ">hello</p>
+      <Button variant={"destructive"} type="submit">
+        click me
+      </Button>
+    </div>
+  );
 }
