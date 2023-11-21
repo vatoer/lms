6805c46ac355441be6bcf0c7fee76f8b080838a4
Fathur Rohman <vatoer.ckplus@gmail.com>
2023-11-21 21:27:01 +0100

    check dif

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,57 @@
-const ChapterIdPage = () => {
-  return <div>chapter id</div>;
+import { getChapter } from "@/actions/get-chapters";
+import Banner from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
+  if (!chapter || !course) {
+    console.error("chapter or course not found");
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.completed;
+
+  return (
+    <div>
+      {userProgress?.completed && (
+        <Banner
+          variant={"success"}
+          label="you already completed this chapter"
+        />
+      )}
+      {isLocked && (
+        <Banner
+          variant={"warning"}
+          label="you need to purchase this course to view this chapter"
+        />
+      )}
+    </div>
+  );
 };
 
 export default ChapterIdPage;
--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,20 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+    ],
+  });
+
+//ts.ignore
+// db.$on("query", async (e) => {
+//   console.log(`${e.query} ${e.params}`);
+// });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
